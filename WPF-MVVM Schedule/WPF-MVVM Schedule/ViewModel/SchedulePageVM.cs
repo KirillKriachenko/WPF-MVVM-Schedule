af3504fcdb0d881637f95ead0948784bb2b67130
KirillKriachenko <kirillkriachenko@gmail.com>
2018-03-27 14:58:56 -0700

    SchedulePageVM
    DrawTable

--- WPF-MVVM Schedule/WPF-MVVM Schedule/ViewModel/SchedulePageVM.cs
+++ WPF-MVVM Schedule/WPF-MVVM Schedule/ViewModel/SchedulePageVM.cs
@@ -30,13 +30,72 @@ namespace WPF_MVVM_Schedule.ViewModel
             gridShedule.MinWidth = 1100;
             gridShedule.MaxWidth = 1200;
 
-            //FindeShedule(gridShedule);
+            FindeShedule(gridShedule);
         }
 
         public void FindeShedule(Grid gridShedule)
         {
-            
+            for (int i = 0; i < 10; i++)
+            {
+                gridShedule.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                gridShedule.ColumnDefinitions.Add(new ColumnDefinition());
+            }
             // draw grid here
+
+            for (int j = 0; j < gridShedule.ColumnDefinitions.Count - 1; j++)
+            {
+                var textBlock = new TextBlock();
+                textBlock.VerticalAlignment = VerticalAlignment.Center;
+
+                textBlock.HorizontalAlignment = HorizontalAlignment.Center;
+
+                textBlock.FontSize = 20;
+
+                gridShedule.Children.Add(textBlock);
+                textBlock.Text = "Table"; // EmployeeCollection[j].FIO;
+                //textBlock.Text = "Кресло  " + Convert.ToString(j + 1);
+                //if (j >= 2)
+                //{
+                //    textBlock.Text = "Дополнительно ";
+                //}
+                Grid.SetColumn(textBlock, j + 1);
+                Grid.SetRow(textBlock, 0);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    var border = new Border();
+                    border.BorderThickness = new Thickness(1, 1, 1, 1);
+                    border.BorderBrush = Brushes.Black;
+
+
+                    var control = new ControlSchedule(i);
+                    control.Visibility = Visibility.Visible;
+                    control.BorderThickness = new Thickness(1, 1, 1, 1);
+                    control.BorderBrush = Brushes.Black;
+                    control.ImageDelete.Visibility = Visibility.Hidden;
+                    control.ImageEdit.Visibility = Visibility.Hidden;
+                    if (j % 4 == 0)
+                    {
+                        control.Background = Brushes.RosyBrown;
+                    }
+
+                    gridShedule.Children.Add(border);
+                    gridShedule.Children.Add(control);
+
+                    Grid.SetColumn(border, i);
+                    Grid.SetRow(border, j);
+
+                    Grid.SetRow(control, j + 1);
+                    Grid.SetColumn(control, i + 1);
+
+                }
+            }
         }
 
     }
